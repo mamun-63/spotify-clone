ed1368bcd41ee5587714e7ef8fe47304cb6f798a
Mamun <mamun.cse63@gmail.com>
2020-09-21 03:29:56 +0600

    provide security to access token and if get, push to another page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 
 function App() {
+  // in react, thats how we handle variables  using state 
+  const [token, setToken] = useState(null)
+
+  // when hit the login> Agree button, App component loads and this code will fire
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = "" // you cant see the access token in url now for security reason, thats cool
+    const _token = hash.access_token  // thats standerd to use _token as we have in useState() a token variable
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("Access Token > ", token)
+  }, [])
+
   return (
     // BEM
     <div className="app">
-      <Login />
+      {/* thats how we write Javascript code inside the return - {} */}
+      { token ? <h1>I am logged in</h1> : <Login /> }
 
     </div>
   );
--- src/spotify.js
+++ src/spotify.js
@@ -13,7 +13,21 @@ const scopes = [
   "user-modify-playback-state",
 ]
 
-
+// login > hitting confirm > response back the url with access token
+// localhost:3000/#access_token=BQAaoOWb9OaxXKTpNg3IGhHB9eYu8N8AMmKQjXSJ45WaqDJYU51EbB90mS02aWePCK7vlISj6oGJHi8RO9UNOX5ll6tJtRcvztW0LxsCfqU9wgluuka8uGA_VprcR26RyvkAjhaz5hTGtQcCvUvafNZvFS-m01x9WFCpE3boCzViyAZK&token_type=Bearer&expires_in=3600 
+// localhost:3000/#access_token=mySuperSecretkey&token_type=Bearer&expires_in=3600 
+// we have to pull that access token now from url
+export const getTokenFromUrl = () => {
+  return window.location.hash 
+    .substring(1) 
+    .split('&') 
+    .reduce((initial, item) => {
+      let parts = item.split('=')
+      initial[parts[0]] = decodeURIComponent(parts[1])
+      
+      return initial
+    }, {})  // {} this empty object for initial value in reduce rules
+}
 
 // `` backtick is string template, inside you can use javascript and string also
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
