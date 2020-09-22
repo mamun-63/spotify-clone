4db37c31ad1779a112232021b6e74a16bc604649
Mamun <mamun.cse63@gmail.com>
2020-09-22 17:21:39 +0600

    build the banner in body section

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,15 @@ function Body({spotify}) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src="https://picsum.photos/400" alt=""/>
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>description...</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,7 @@ export const initialState = {
   user: null,
 
   // change token: null, after finished developing, this is doing just for our convenience 
-  // token: 'BQBgpcYgQFCyJGMlV0yTOWD4CF-x-At_XPhznQwuDbb9WqRK_CWnmkHVHYMnveITC9-ukjLwCtsAgjXu8yl7yrH289fp0QZ7mNHow0BAt7eO95eyEoG9OPRz6K6mlSyEV76usyHrHZCYtkXpBh7BGxxJQoBjRsOxywOMwwFMvTlv4hlx',
+  token: 'BQBgpcYgQFCyJGMlV0yTOWD4CF-x-At_XPhznQwuDbb9WqRK_CWnmkHVHYMnveITC9-ukjLwCtsAgjXu8yl7yrH289fp0QZ7mNHow0BAt7eO95eyEoG9OPRz6K6mlSyEV76usyHrHZCYtkXpBh7BGxxJQoBjRsOxywOMwwFMvTlv4hlx',
 
   playlists: [],
   playing: false,
